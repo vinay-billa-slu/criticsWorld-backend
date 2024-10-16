1b2555c89226ca3c170cc68c727d88f19967e6d4
Mohansai Aavulapati <mohansaiaavulapati@Mohansais-Laptop.local>
2024-10-16 00:02:02 -0500

    Pushing Test Cases for Database Connection

---
+++ config/Testcases.test.js
@@ -0,0 +1,18 @@
+const { connectToDatabase } = require("./db");
+const mysql = require("mysql2/promise");
+
+jest.mock("mysql2/promise", () => ({
+  createPool: jest.fn(() => ({
+    getConnection: jest.fn().mockResolvedValue({
+      release: jest.fn(),
+    }),
+  })),
+}));
+
+test("should successfully connect to the database and release the connection", async () => {
+  const connection = await connectToDatabase();
+  
+  expect(mysql.createPool).toHaveBeenCalled();
+  expect(connection.release).toHaveBeenCalled();
+});
+
