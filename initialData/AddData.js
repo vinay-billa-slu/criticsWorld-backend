e2c090c201b022ed0e444ef6139c31dbbe658d00
VenugopalRao9 <venurao0100@gmail.com>
2024-11-13 23:57:02 -0600

    AddData file added

--- initialData/AddData.js
+++ initialData/AddData.js
@@ -7,4 +7,30 @@ fs.readFile(jsonFilePath, "utf8", async (err, data) => {
     console.error("Error reading JSON file:", err);
     return;
   }
+
+try {
+    const jsonData = JSON.parse(data);
+    console.log(jsonData);
+
+    for (let i = 0; i < jsonData.length; i++) {
+      const e = jsonData[i];
+
+      const q =
+        "INSERT INTO Movie (Title, ReleaseYear, PosterImage, Actors, Director) VALUES (?);";
+
+      const values = [
+        e.title,
+        e.date,
+        e.image,
+        "Leonardo DiCaprio, Tom Cruise, Johnny Depp, Chris Hemsworth",
+        "James Cameron",
+      ];
+
+      const connection = await connectToDatabase();
+      await connection.query(q, [values]);
+      console.log(i);
+    }
+  } catch (jsonError) {
+    console.error("Error parsing JSON:", jsonError);
+  }
 });
