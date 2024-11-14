81be73d6d6b0c6ffbb44ae462a6e8c65511feda7
Gayathri Gurram <gayathri@Gayathris-Laptop.local>
2024-11-13 23:59:00 -0600

    pushing Review file complete one

--- routes/Review.js
+++ routes/Review.js
@@ -20,5 +20,37 @@ router.post("/getAllReviews", verifyToken, async (req, res, next) => {
   }
 });
 
+router.post("/getReview", verifyToken, async (req, res, next) => {
+  try {
+    const result = await reviewController.getReview(req.body);
+    res.status(200).send({ success: true, data: result });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/deleteReview", verifyToken, async (req, res, next) => {
+  try {
+    await reviewController.deleteReview(req.body, req.user);
+    res.status(200).send({ success: true, message: "Review has been deleted" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/updateReview", verifyToken, async (req, res, next) => {
+  try {
+    const result = await reviewController.updateReview(req.body, req.user);
+    res
+      .status(200)
+      .send({
+        success: true,
+        message: "Review has been updated",
+        data: result,
+      });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
