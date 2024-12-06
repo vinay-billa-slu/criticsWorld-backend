25a3eff6956b5479ab63f84973e23a6c2597d0d6
Gayathri Gurram <gayathri@Gayathris-Laptop.local>
2024-12-06 02:10:39 -0600

    pushing  file with updated review part in review

--- routes/Review.js
+++ routes/Review.js
@@ -7,4 +7,13 @@ router.get("/ping", (req, res) => {
   res.status(200).send({ success: true, message: "Server is running" });
 });
 
-
+// Add a review
+router.post("/addReview", verifyToken, async (req, res, next) => {
+  try {
+    console.log("Adding review:", req.body); // Log the incoming request
+    await reviewController.addReview(req.body);
+    res.status(200).send({ success: true, message: "Review Added" });
+  } catch (error) {
+    next(error);
+  }
+});
