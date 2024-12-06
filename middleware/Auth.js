76ed9feda8953094f1adb076411d598c8660833e
Mohansai Aavulapati <mohansaiaavulapati@Mohansais-Laptop.local>
2024-12-05 22:59:54 -0600

    updating Auth file in middleware

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -3,18 +3,9 @@ const config = require("../config");
 
 const verifyToken = (request, response, next) => {
   const authToken = request.headers.token;
-  if (authToken) {
-    jwt.verify(authToken, config.JWT_SEC_KEY, (error, user) => {
-      if (error) {
-        return response.status(401).json({
-          success: false,
-          message: "You are not authenticated.",
-        });
-      }
-      request.user = user;
-      next();
-    });
-  } else {
+  
+  // Use early return for cleaner flow
+  if (!authToken) {
     return response.status(401).json({
       success: false,
       message: "You are not authenticated.",
@@ -22,48 +13,3 @@ const verifyToken = (request, response, next) => {
   }
 };
 
-const verifyTokenAndAdmin = (request, response, next) => {
-  verifyToken(request, response, () => {
-    if (request.user.isAdmin) {
-      next();
-    } else {
-      return response.status(401).json({
-        success: false,
-        message: "You are not allowed.",
-      });
-    }
-  });
-};
-
-const verifyTokenAndAuthorization = (request, response, next) => {
-  verifyToken(request, response, () => {
-    if (request.user.id === request.params.id || request.user.isAdmin) {
-      next();
-    } else {
-      return response.status(401).json({
-        success: false,
-        message: "You are not allowed.",
-      });
-    }
-  });
-};
-
-const userWithToken = (request, response, next) => {
-  const authToken = request.headers.token;
-  if (authToken) {
-    jwt.verify(authToken, config.JWT_SEC_KEY, (error, user) => {
-      if (user) {
-        request.user = user;
-      }
-      next();
-    });
-  }
-  next();
-};
-
-module.exports = {
-  verifyToken,
-  verifyTokenAndAdmin,
-  verifyTokenAndAuthorization,
-  userWithToken,
-};
