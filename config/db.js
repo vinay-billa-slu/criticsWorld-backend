f760dbd6a410274555536b4cc3d8466820b175ce
Vinay Billa <vinay.billa@slu.edu>
2025-03-26 17:56:07 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -19,37 +19,34 @@
 // };
 
 // module.exports = { connectToDatabase };
+
 const mysql = require("mysql2/promise");
 const config = require("./index");
 const HttpException = require("../middleware/HttpException");
-
+require('dotenv').config()
 const environment = process.env;
 
-// Connection pool configuration
+console.log("env", process.env.DB_HOST);
+
 const pool = mysql.createPool({
-  host: environment.DB_HOST || config.HOST,
-  user: environment.DB_USER || config.USER,
-  port: environment.DB_PORT || config.DB_PORT,
-  password: environment.DB_PASSWORD || config.PASSWORD,
-  database: environment.DATABASE || config.DATABASE,
+  host: environment.DB_HOST ? environment.DB_HOST : config.HOST,
+  user: environment.DB_USER ? environment.DB_USER : config.USER,
+  port: config.DB_PORT,
+  password: environment.DB_PASSWORD ? environment.DB_PASSWORD : config.PASSWORD,
+  database: environment.DATABASE ? environment.DATABASE : config.DATABASE,
   waitForConnections: true,
-  connectionLimit: 10,  // Adjust this value as needed
+  // connectionLimit: 10, // Adjust according to your needs
   queueLimit: 0,
 });
 
-// Function to get a database connection
 const connectToDatabase = async () => {
   try {
     const connection = await pool.getConnection();
-    
-    // Return a connection with `query` and `release` methods
-    return {
-      query: connection.query.bind(connection),
-      release: () => connection.release(),
-    };
+    connection.release();
+    return connection;
   } catch (err) {
     console.error("Error in database connection:", err.message);
-    throw new HttpException(false, 500, "Error in database connection");
+    throw new HttpException(false, 400, "Error in database connection");
   }
 };
 
